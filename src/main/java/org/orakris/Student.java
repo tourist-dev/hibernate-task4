64b470db7b22350cd1db87f9aa64ca120fed3c92
tourist-dev <shaik.sameer@orakris.com>
2021-05-26 16:57:36 +0530

    created Student.java and Teacher.java

---
+++ src/main/java/org/orakris/Student.java
@@ -0,0 +1,41 @@
+package org.orakris;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+
+    @ManyToMany(targetEntity = Teacher.class, cascade = {CascadeType.ALL})
+    @JoinTable(name = "teacher", joinColumns = {@JoinColumn(name = "s_id")}, inverseJoinColumns = { @JoinColumn(name = "t_id") })
+    private List<Teacher> teacherList;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Teacher> getTeacherList() {
+        return teacherList;
+    }
+
+    public void setTeacherList(List<Teacher> teacherList) {
+        this.teacherList = teacherList;
+    }
+}
